c5a7f21cc3f14be2f4f18c95dbcbcee88da754a7
jlkiri <mail@kirillvasiltsov.com>
2023-09-01 18:30:36 +0900

    error

--- src/adsb.rs
+++ src/adsb.rs
@@ -1,63 +1,64 @@
 
 
-use nom::{IResult, bytes::complete::{take, tag}, bits, sequence::{preceded, tuple}, combinator::{cond, rest, all_consuming, recognize}, Err, multi::count, Finish};
+use nom::{IResult, bytes::complete::{take, tag}, bits, sequence::{preceded, tuple}, combinator::{cond, rest, all_consuming, recognize}, Err, multi::count, Finish, error::{ParseError, ErrorKind}};
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 const ADS_B_DOWNLINK_FORMAT: u8 = 17;
 const ADS_B_CAPABILITY: u8 = 5;
-const TYPE_CODE_IDENTIFICATION: u8 = 4;
+const TYPECODE_IDENTIFICATION: u8 = 4;
 
-fn parse_df_ca(input: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, u8), ()> {
+
+#[derive(Debug)]
+pub struct ADSBFrame {
+    downlink_fmt: u8,
+    capability: u8,
+    icao: String,
+    typecode: u8,
+    callsign: String,
+}
+
+#[derive(Debug)]
+struct AdsbParseError(String);
+
+impl<I> ParseError<I> for AdsbParseError {
+    fn from_error_kind(input: I, kind: ErrorKind) -> Self {
+        AdsbParseError("not an ads-b frame".into())
+    }
+
+    // if combining multiple errors, we show them one after the other
+    fn append(input: I, kind: ErrorKind, other: Self) -> Self {
+        other
+    }
+
+    fn from_char(input: I, c: char) -> Self {
+        AdsbParseError("not an ads-b frame".into())
+    }
+
+    fn or(self, other: Self) -> Self {
+        AdsbParseError("not an ads-b frame".into())
+    }
+}
+
+fn header(input: &[u8]) -> IResult<&[u8], &[u8], AdsbParseError> {
+    let header = tuple((tag([0x1a]), tag([0x33]), take(6u8), take(1u8)));
+    recognize(header)(input)
+}
+
+fn df_ca(input: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, u8), AdsbParseError> {
     let ((input, offset), df) = bits::complete::take(5u8)(input)?;
     let ((input, offset), ca) = bits::complete::take(3u8)((input, offset))?;
     assert!(offset == 0);
     Ok(((input, offset), (df, ca)))
 }
 
-fn parse_icao(input: &[u8]) -> IResult<&[u8], String, ()> {
+fn icao(input: &[u8]) -> IResult<&[u8], String, AdsbParseError> {
     let (input, icao) = take(3u8)(input)?;
     Ok((input, format!("{:02x}{:02x}{:02x}", icao[0], icao[1], icao[2])))
 }
 
-#[derive(Debug)]
-pub struct ADS_B_Frame {
-    df: u8,
-    ca: u8,
-    icao: String,
-    tc: u8,
-    callsign: String,
-}
 
-fn parse_adsb_frame(input: &[u8]) -> IResult<&[u8], ADS_B_Frame, ()> {
-    let (input, _) = parse_header(input)?;
-    let ((input, _), df_ca) = parse_df_ca((input, 0))?;
-    if df_ca != (ADS_B_DOWNLINK_FORMAT, ADS_B_CAPABILITY) {
-        return Err(Err::Failure(()));
-    }
-
-    let (input, icao) = parse_icao(input)?;
-    let ((input, _), tc_ca) = parse_tc_ca((input, 0))?;
-    if tc_ca.0 != TYPE_CODE_IDENTIFICATION {
-        return Err(Err::Failure(()));
-    }
-
-    let (input, callsign) = parse_callsign(input)?;
-    Ok((input, ADS_B_Frame {
-        df: df_ca.0,
-        ca: df_ca.1,
-        icao,
-        tc: tc_ca.0,
-        callsign,
-    }))
-}
-
-pub fn pub_parse_adsb_frame(input: &[u8]) -> Result<ADS_B_Frame> {
-    let frame = parse_adsb_frame(input).finish().map(|(_, frame)| frame).map_err(|_| "parse error")?;
-    return Ok(frame)
-}
-
-fn parse_callsign(input: &[u8]) -> IResult<&[u8], String, ()> {
+fn callsign(input: &[u8]) -> IResult<&[u8], String, AdsbParseError> {
     // https://mode-s.org/decode/content/ads-b/2-identification.html
     let ((input, offset), mut chunks) = count(nom::bits::complete::take(6u8), 8)((input, 0))?;
     assert!(offset == 0);
@@ -66,19 +67,43 @@ fn parse_callsign(input: &[u8]) -> IResult<&[u8], String, ()> {
             *chunk |= 0x40;
         }
     });
-    Ok((input, String::from_utf8_lossy(&chunks).into_owned()))
+    Ok((input, String::from_utf8_lossy(&chunks).trim_end().to_owned()))
 }
 
-fn parse_tc_ca(input: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, u8), ()> {
+fn typecode_category(input: (&[u8], usize)) -> IResult<(&[u8], usize), (u8, u8), AdsbParseError> {
     let ((input, offset), tc) = bits::complete::take(5u8)(input)?;
     let ((input, offset), ca) = bits::complete::take(3u8)((input, offset))?;
     assert!(offset == 0);
     Ok(((input, offset), (tc, ca)))
 }
 
-fn parse_header(input: &[u8]) -> IResult<&[u8], &[u8], ()> {
-    let header = tuple((tag([0x1a]), tag([0x33]), take(6u8), take(1u8)));
-    recognize(header)(input)
+
+fn adsb_frame(input: &[u8]) -> IResult<&[u8], ADSBFrame, AdsbParseError> {
+    let (input, _) = header(input)?;
+    let ((input, _), df_ca) = df_ca((input, 0))?;
+    if df_ca != (ADS_B_DOWNLINK_FORMAT, ADS_B_CAPABILITY) {
+        return Err(Err::Failure(AdsbParseError("not an ads-b frame".into())));
+    }
+
+    let (input, icao) = icao(input)?;
+    let ((input, _), tc_ca) = typecode_category((input, 0))?;
+    if tc_ca.0 != TYPECODE_IDENTIFICATION {
+        return Err(Err::Failure(AdsbParseError("not an ads-b frame".into())));
+    }
+
+    let (input, callsign) = callsign(input)?;
+    Ok((input, ADSBFrame {
+        downlink_fmt: df_ca.0,
+        capability: df_ca.1,
+        icao,
+        typecode: tc_ca.0,
+        callsign,
+    }))
+}
+
+pub fn parse_adsb_frame(input: &[u8]) -> Result<ADSBFrame> {
+    let frame = adsb_frame(input).finish().map(|(_, frame)| frame).map_err(|e| e.0)?;
+    return Ok(frame)
 }
 
 #[cfg(test)]
@@ -87,7 +112,7 @@ mod tests {
     #[test]
     fn test_parse_df_ca() -> Result<()> {
         let input = [0x8d];
-        let (_, df_ca) = parse_df_ca((&input, 0))?;
+        let (_, df_ca) = df_ca((&input, 0))?;
         assert_eq!(df_ca, (17, 5));
         Ok(())
     }
@@ -95,7 +120,7 @@ mod tests {
     #[test]
     fn test_parse_icao() -> Result<()> {
         let input = [0x84, 0x1b, 0xd1];
-        let (_, icao) = parse_icao(&input)?;
+        let (_, icao) = icao(&input)?;
         assert_eq!(icao, "841bd1");
         Ok(())
     }
@@ -103,7 +128,7 @@ mod tests {
     #[test]
     fn test_parse_tc_ca() -> Result<()> {
         let input = [0x20];
-        let (_, tc_ca) = parse_tc_ca((&input, 0))?;
+        let (_, tc_ca) = typecode_category((&input, 0))?;
         assert_eq!(tc_ca, (4, 0));
         Ok(())
     }
@@ -111,7 +136,7 @@ mod tests {
     #[test]
     fn test_parse_callsign() -> Result<()> {
         let input = [0x2C, 0xC3, 0x71, 0xC3, 0x2C, 0xE0];
-        let (_, callsign) = parse_callsign(&input)?;
+        let (_, callsign) = callsign(&input)?;
         assert_eq!(callsign, "KLM1023 ");
         Ok(())
     }
@@ -119,11 +144,11 @@ mod tests {
     #[test]
     fn test_parse_adsb_frame() -> Result<()> {
         let input = [0x1a, 0x33, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x8d, 0x84, 0x1b, 0xd1, 0x20, 0x2C, 0xC3, 0x71, 0xC3, 0x2C, 0xE0];
-        let (_, frame) = parse_adsb_frame(&input)?;
-        assert_eq!(frame.df, 17);
-        assert_eq!(frame.ca, 5);
+        let (_, frame) = adsb_frame(&input)?;
+        assert_eq!(frame.downlink_fmt, 17);
+        assert_eq!(frame.capability, 5);
         assert_eq!(frame.icao, "841bd1");
-        assert_eq!(frame.tc, 4);
+        assert_eq!(frame.typecode, 4);
         assert_eq!(frame.callsign, "KLM1023 ");
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -10,45 +10,10 @@ fn main() -> Result<(), std::io::Error> {
 
     loop {
         let _n = client.read(&mut buf)?;
-        if let Ok(frame) =  adsb::pub_parse_adsb_frame(&buf) {
+        if let Ok(frame) =  adsb::parse_adsb_frame(&buf) {
             println!("{:?}", frame);
         }
     }
-
-    // loop {
-    //     let _n = client.read(&mut buf)?;
-    //     let mut iter = buf.iter();
-    //     assert!(*iter.next().unwrap() == 0x1a);
-    //     let frame_type = *iter.next().unwrap(); 
-    //     if frame_type == 0x33 {
-            
-    //         let skip_timestamp = iter.skip(6);
-    //         let mut skip_rssi = skip_timestamp.skip(1);
-    //         let dfca = *skip_rssi.next().unwrap();
-    //         if dfca == 0x8d {
-    //             let mut skip_icao = skip_rssi.skip(3);
-    //             let tc_ca = *skip_icao.next().unwrap();
-    //             // println!("TC CA: {} {}", tc_ca >> 3, tc_ca << 5);
-    //             if (1..=4).contains(&(tc_ca >> 3)) && tc_ca << 5 == 0 {
-    //                 let callsign = skip_icao.take(8).copied().collect::<Vec<u8>>();
-    //                 let mut br = BitReader::new(&callsign);
-    //                 let mut buf = [0u8; 8];
-    //                 for byte in &mut buf {
-    //                     let b = br.read_u8(6).expect("bitreader error");
-    //                     if b < 26 {
-    //                         *byte = b | 0x40;
-    //                     } else {
-    //                         *byte = b;
-    //                     }
-    //                 }
-                    
-    //                 print!("callsign: {} ",core::str::from_utf8(&buf).expect("utf8 error"));
-                    
-    //                 println!();
-    //             }
-    //         }
-    //     }
-    // }
 
     // Ok(())
 }
